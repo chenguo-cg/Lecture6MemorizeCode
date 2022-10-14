faeb61e61fb563de7c586e7add5e421b401d0a65
guochen <chenguo0313@gmail.com>
2022-10-14 06:49:16 -0400

    Initial Commit

---
+++ Lecture6MemorizeCode/Lecture6MemorizeCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture6MemorizeCodeApp.swift
+//  Lecture6MemorizeCode
+//
+//  Created by Guo Chen on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Lecture6MemorizeCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
